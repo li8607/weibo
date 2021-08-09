2c2bcfc34fe47d321b6ce3b593d8866aa38b8db9
limingfei <675669728@qq.com>
2021-08-09 16:21:08 +0800

    初始化样式

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,8 +1,18 @@
 <html>
     <head>
         <title>@yield('title', 'Weibo App') - Laravel 新手入门教程</title>
+        <link rel="stylesheet" href="/css/app.css" >
     </head>
     <body>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div class="container">
+                <a class="navbar-brand" href="/">Weibo App</a>
+                <ul class="navbar-nav justify-content-end">
+                    <li class="navbar-item"><a class="nav-link" href="/help">帮助</a></li>
+                    <li class="navbar-item"><a class="nav-link" href="#">登录</a></li>
+                </ul>
+            </div>
+        </nav>
         @yield('content')
     </body>
 </html>
